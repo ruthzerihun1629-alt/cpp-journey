664dc38a01eec13f158bd752c8a97f77133c672d
Ruth Zerihun <ruthzerihun1629@gmail.com>
2025-10-09 17:23:03 +0300

    first commit

---
+++ firstprogram.cpp
@@ -0,0 +1,11 @@
+#include<iostream>
+using namespace std;
+int main() {
+int mynum=21;
+char letter='a';
+double height=5.11;
+string text="hope";
+bool mark=true;
+cout<<"mr.jhon is" << mynum << "his height is"<<height<<"he is"<<letter<<mark<<text;
+return 0;
+}
