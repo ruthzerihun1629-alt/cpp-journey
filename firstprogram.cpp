64da219f0cb28460664c7d44739a1f1e2282d38a
Ruth Zerihun <ruthzerihun1629@gmail.com>
2025-10-12 02:10:22 +0300

    Updated Day 1 variables with new examples

--- firstprogram.cpp
+++ firstprogram.cpp
@@ -1,11 +1,24 @@
 #include<iostream>
-using namespace std;
-int main() {
-int mynum=21;
-char letter='a';
-double height=5.11;
-string text="hope";
-bool mark=true;
-cout<<"mr.jhon is" << mynum << "his height is"<<height<<"he is"<<letter<<mark<<text;
+namespace first{
+    double gpa =3.1;
+}
+//typedef std::string text_t;
+//using text_t= std::string;
+int main(){
+ int student= 15+45-1*4/3;
+char symbol='$';
+const bool question= true;
+double gpa=2.8;
+float price=66.54;
+
+std::cout<<"hello " << " your gpa is " <<first::gpa <<" it is " << question <<" the price is " << price << symbol; 
+//student+=1;
+int reminder= student % 2;
+std::cout<<reminder; 
+std::cout<<student;
+std::string name;
+std::cout<<"what is your name:";
+std::cin>>name;
+std::cout<<"hii "<< name;
 return 0;
 }
